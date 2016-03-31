334819dfee4ded68f66dc5748c423a78a469093b
David Buschtöns <dbuschtoens@eclipsesource.com>
2016-03-31 17:21:50 +0200

    Fix spacer being applied to wrong widget

--- src/forcastScroll.ts
+++ src/forcastScroll.ts
@@ -1,75 +1,87 @@
 /// <reference path="../node_modules/tabris/tabris.d.ts"/>
-import {WeatherData} from "./weatherService";
+import {WeatherData, WeatherDatum} from "./weatherService";
+const textColor = "rgb(255, 255,255)";
+const margin = 8;
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const forcastBoxWidth = 90;
+const forcastBoxHeight = 120;
+const smallFont = "15px";
 
-export default class ForcastScroll extends tabris.Composite {
-  constructor(data: WeatherData, properties: Object) {
+export default class ForcastScrollView extends tabris.ScrollView {
+  private scrollView: tabris.ScrollView;
+
+  constructor(data: WeatherData, properties: any) {
+    properties.direction = "horizontal";
     super(properties);
-    const textColor = "rgb(255, 255,255)";
-    const margin = 8;
-    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    const forcastBoxWidth = 90;
-    const forcastBoxHeight = 120;
-    const smallFont = "15px";
-    let scroll = new tabris.ScrollView({
-      top: "prev()",
-      left: 0,
-      right: 0,
-      direction: "horizontal"
-    });
-    scroll.appendTo(this);
-    for (let index = 0; index < data.forcastCount; index++) {
-      let forcastContainer = new tabris.Composite({
-        top: 0,
-        left: "prev()",
-      });
-      forcastContainer.appendTo(scroll);
-      let forcastDetail = new tabris.Composite({
-        top: margin,
-        left: margin,
-        width: forcastBoxWidth,
-        height: forcastBoxHeight,
-        background: "rgba(26, 26, 26, 0.3)"
-      });
-      forcastDetail.appendTo(forcastContainer);
-      let dayText = new tabris.TextView({
-        top: 0,
-        centerX: 0,
-        text: days[data.forcasts[index].date.getDay()] + " " + data.forcasts[index].date.getDate(),
-        textColor: textColor,
-        font: smallFont
-      });
-      dayText.appendTo(forcastDetail);
-      let timeText = new tabris.TextView({
-        top: "prev()",
-        centerX: 0,
-        text: data.forcasts[index].date.getHours() + ":00",
-        textColor: textColor,
-        font: "italic " + smallFont
-      });
-      timeText.appendTo(forcastDetail);
-      let weatherIcon = new tabris.ImageView({
-        bottom: 0, // "prev() -10",
-        centerX: 0,
-        image: "/icons/" + data.forcasts[index].weatherIcon + ".png"
-      });
-      weatherIcon.appendTo(forcastDetail);
-      let temperatureText = new tabris.TextView({
-        bottom: "prev() -10",
-        centerX: 0,
-        text: Math.round(data.forcasts[index].temperature) + "°C",
-        textColor: textColor,
-        font: "bold 28px"
-      });
-      temperatureText.appendTo(forcastDetail);
-      if (index > 0 && data.forcasts[index].date.getDay() === data.forcasts[index - 1].date.getDay()) {
-        // only show day on first forcast of that day
-        dayText.set("text", "");
-      }
-      let spacer = new tabris.Composite({
-        // makes it so that the scrollbar doesn't overlap with the forcast detail boxes  
-        top: "prev()",
-        height: 10,
-      }).appendTo(forcastContainer);
+    this.createForcastBox(data.forcasts[0]).set("left", 0).appendTo(this);
+    for (let index = 1; index < data.forcasts.length; index++) {
+      this.createForcastBox(data.forcasts[index], data.forcasts[index - 1]).appendTo(this);
     }
+  }
+
+  createForcastBox(forcast: WeatherDatum, previous?: WeatherDatum) {
+    let container = new tabris.Composite({
+      top: 0,
+      left: "prev()",
+    });
+    let forcastBox = new tabris.Composite({
+      top: margin,
+      left: margin,
+      width: forcastBoxWidth,
+      height: forcastBoxHeight,
+      background: "rgba(226, 226, 226, 0.3)"
+    });
+    forcastBox.appendTo(container);
+    let dayText = this.createDayText(forcast).appendTo(forcastBox);
+    if ((previous) && forcast.date.getDay() === previous.date.getDay()) {
+      dayText.set("text", "");
+    }
+    this.createTimeText(forcast).appendTo(forcastBox);
+    this.createWeatherIcon(forcast).appendTo(forcastBox);
+    this.createTemperatureText(forcast).appendTo(forcastBox);
+    this.createSpacer().appendTo(container);
+    return container;
+  }
+  createDayText(forcast) {
+    return new tabris.TextView({
+      top: 0,
+      centerX: 0,
+      text: days[forcast.date.getDay()] + " " + forcast.date.getDate(),
+      textColor: textColor,
+      font: smallFont
+    });
+  }
+  createTimeText(forcast) {
+    return new tabris.TextView({
+      top: "prev()",
+      centerX: 0,
+      text: forcast.date.getHours() + ":00",
+      textColor: textColor,
+      font: "italic " + smallFont
+    });
+  }
+  createWeatherIcon(forcast) {
+    return new tabris.ImageView({
+      bottom: 0,
+      width: 50,
+      height: 50,
+      centerX: 0,
+      image: "/icons/" + forcast.weatherIcon + ".png"
+    });
+  }
+  createTemperatureText(forcast) {
+    return new tabris.TextView({
+      bottom: "prev() -10",
+      centerX: 0,
+      text: Math.round(forcast.temperature) + "°C",
+      textColor: textColor,
+      font: "bold 28px"
+    });
+  }
+  createSpacer() {
+    return new tabris.Composite({
+      top: "prev()",
+      height: 10
+    });
   }
 }
