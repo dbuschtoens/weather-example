c269411451c6860aed4ef126af43d5b4312d945f
David Buschtöns <dbuschtoens@eclipsesource.com>
2016-04-04 15:43:22 +0200

    Change forecastScroll to be vertical and use thin font

--- src/app.ts
+++ src/app.ts
@@ -9,42 +9,16 @@ tabris.ui.set("toolbarVisible", false);
 let page = new tabris.Page({
   title: "Weather Forecast",
   topLevel: true,
+  background: "rgb(75,115,173)"
 });
 
-createBackground().appendTo(page);
-
-let scrollView = new tabris.ScrollView({
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-});
-scrollView.appendTo(page);
-
-/*let citySelector = new tabris.TextInput({
-  top: 0,
-  centerX: 0,
-  message: "Test",
-  font: "bold 32px"
-}).on("accept", (widget) => {
-  console.error("accept");
-}).on("blur", (widget) => {
-  console.error("blur");
-}).on("change:text", (widget) => {
-  console.error("change:text");
-}).on("focus", (widget) => {
-  console.error("focus");
-}).on("input", (widget) => {
-  console.error("input");
-}).appendTo(scrollView);*/
-let citySelector = createCitySelector().appendTo(scrollView)
+let citySelector = createCitySelector().appendTo(page)
 let currentWeatherInformation: tabris.Composite;
-
 
 function drawNewCity(data: WeatherData) {
   citySelector.set("text", data.cityName + ", " + data.countryName);
   if (currentWeatherInformation) currentWeatherInformation.dispose();
-  currentWeatherInformation = <tabris.Composite>createWeatherInformation(data).appendTo(scrollView);
+  currentWeatherInformation = <tabris.Composite>createWeatherInformation(data).appendTo(page);
 }
 
 function createWeatherInformation(data: WeatherData) {
@@ -52,29 +26,11 @@ function createWeatherInformation(data: WeatherData) {
     top: "prev()",
     left: 0,
     right: 0
-  })
-  new CurrentWeatherView({
-    data: data,
-    top: 0,
-    left: 0,
-    right: 0
-  }).appendTo(weatherInformationComposite);
+  });
   let graph = <Graph>new Graph({
     data: data,
-    top: "prev()",
+    top: "top",
     left: 0
-  }).appendTo(weatherInformationComposite);
-  new tabris.Button({
-    top: "prev()",
-    text: "<->"
-  }).on("select", () => {
-    graph.zoom(1.2);
-  }).appendTo(weatherInformationComposite);
-  new tabris.Button({
-    top: "prev()",
-    text: ">-<"
-  }).on("select", () => {
-    graph.zoom(0.8);
   }).appendTo(weatherInformationComposite);
   new ForecastScrollView({
     data: data,
@@ -101,7 +57,7 @@ function createCitySelector() {
     centerX: 0,
     message: "enter city",
     textColor: "#FFFFFF",
-    font: "bold 32px"
+    font: "normal thin 32px sans-serif"
   }).on("focus", (widget) => {
     widget.set("text", "");
   }).on("accept", (widget, text) => {
@@ -114,22 +70,5 @@ function createCitySelector() {
       }).then(() => activityIndicator.dispose());
   });
 }
-
-function oldCitySelector() {
-  return new tabris.TextInput({
-    centerX: 0,
-    message: "enter city"
-  }).on("accept", (widget, text) => {
-    let activityIndicator = new tabris.ActivityIndicator({ centerX: 0, centerY: 0 }).appendTo(page);
-    pollWeatherData(text)
-      .then(drawNewCity)
-      .then(() => widget.dispose())
-      .catch((error) => {
-        console.error(error);
-        widget.set("text", "");
-      }).then(() => activityIndicator.dispose());
-  });
-}
-
 
 page.open();
--- src/forecastScrollView.ts
+++ src/forecastScrollView.ts
@@ -2,10 +2,12 @@
 import {WeatherData, WeatherDatum} from "./weatherService";
 const textColor = "rgb(255, 255,255)";
 const margin = 8;
+const innerMargin = 8;
 const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-const forecastBoxWidth = 90;
-const forecastBoxHeight = 120;
-const smallFont = "15px";
+// const forecastBoxWidth = 90;
+// const forecastBoxHeight = 120;
+const smallFont = "thin 20px sans-serif";
+const temperatureFont = "thin 28px sans-serif";
 
 interface ForecastScrollViewProperties extends tabris.ScrollViewProperties {
   data: WeatherData;
@@ -15,39 +17,72 @@ export default class ForecastScrollView extends tabris.ScrollView {
   private scrollView: tabris.ScrollView;
 
   constructor(properties: ForecastScrollViewProperties) {
-    properties.direction = "horizontal";
+    properties.direction = "vertical";
     super(properties);
     let data = properties.data;
-    this.createForecastBox(data.list[0]).set("left", 0).appendTo(this);
+    this.createForecastBox(data.list[0]).set("top", 0).appendTo(this);
+    // this.createTextExamples();
     for (let index = 1; index < data.list.length; index++) {
-      this.createForecastBox(data.list[index], data.list[index - 1]).appendTo(this);
+      this.createForecastBox(data.list[index]).appendTo(this);
     }
   }
 
-  createForecastBox(forecast: WeatherDatum, previous?: WeatherDatum) {
+  createTextExamples() {
+    let fontStyles = ["normal", "italic"];
+    let fontWeights = ["light", "thin", "normal", "medium", "bold", "black"];
+    let fontFamilies = ["serif", "sans-serif", "condensed", "monospace"];
+    for (let style of fontStyles) {
+      for (let weight of fontWeights) {
+        for (let family of fontFamilies) {
+          this.createTextExample(style + " " + weight + " 20px " + family).appendTo(this);
+        }
+      }
+    }
+  }
+
+  createTextExample(text: string) {
+    let [r, g, b] = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)];
     let container = new tabris.Composite({
-      top: 0,
-      left: "prev()",
+      top: "prev()",
+      left: margin,
+      right: margin
     });
-    let forecastBox = new tabris.Composite({
+    let forecastBox = <tabris.Composite>new tabris.Composite({
       top: margin,
       left: margin,
-      width: forecastBoxWidth,
-      height: forecastBoxHeight,
-      background: "rgba(226, 226, 226, 0.3)"
-    });
-    forecastBox.appendTo(container);
-    let dayText = this.createDayText(forecast).appendTo(forecastBox);
-    if ((previous) && forecast.date.getDay() === previous.date.getDay()) {
-      dayText.set("text", "");
-    }
-    this.createTimeText(forecast).appendTo(forecastBox);
-    this.createWeatherIcon(forecast).appendTo(forecastBox);
-    this.createTemperatureText(forecast).appendTo(forecastBox);
-    this.createSpacer().appendTo(container);
+      right: margin,
+      background: "rgba(255,255,255, 0.2)"
+    }).appendTo(container);
+    new tabris.TextView({
+      top: innerMargin,
+      left: innerMargin,
+      bottom: innerMargin,
+      text: text,
+      textColor: textColor,
+      font: text
+    }).appendTo(forecastBox);
     return container;
   }
-  createDayText(forecast) {
+
+  createForecastBox(forecast: WeatherDatum) {
+    let container = new tabris.Composite({
+      top: "prev()",
+      left: margin,
+      right: margin
+    });
+    let forecastBox = <tabris.Composite>new tabris.Composite({
+      top: margin,
+      left: margin,
+      right: margin,
+      background: "rgba(255, 255, 255, 0.2)"
+    }).appendTo(container);
+    this.createTimeText(forecast).appendTo(forecastBox);
+    this.createWeatherText(forecast).appendTo(forecastBox);
+    this.createTemperatureText(forecast).appendTo(forecastBox);
+    this.createWeatherIcon(forecast).appendTo(forecastBox);
+    return container;
+  }
+  createDayText(forecast: WeatherDatum) {
     return new tabris.TextView({
       top: 0,
       centerX: 0,
@@ -56,33 +91,50 @@ export default class ForecastScrollView extends tabris.ScrollView {
       font: smallFont
     });
   }
-  createTimeText(forecast) {
+  createTimeText(forecast: WeatherDatum) {
+    let minutes = forecast.date.getMinutes();
+    let hours = forecast.date.getHours();
+    let hoursString = (hours < 10) ? "0" + hours : hours;
+    let minutesString = (minutes < 10) ? "0" + minutes : minutes;
     return new tabris.TextView({
-      top: "prev()",
-      centerX: 0,
-      text: forecast.date.getHours() + ":00",
+      top: innerMargin,
+      bottom: innerMargin,
+      left: innerMargin,
+      text: hoursString + ":" + minutesString,
       textColor: textColor,
-      font: "italic " + smallFont
+      font: smallFont
     });
   }
-  createWeatherIcon(forecast) {
+  createWeatherText(forecast: WeatherDatum) {
+    return new tabris.TextView({
+      top: innerMargin,
+      bottom: innerMargin,
+      left: "prev() 10",
+      text: forecast.weatherDetailed,
+      textColor: textColor,
+      font: smallFont
+    });
+  }
+  createTemperatureText(forecast: WeatherDatum) {
+    return new tabris.TextView({
+      right: margin,
+      centerY: 0,
+      text: Math.round(forecast.temperature) + "°C",
+      textColor: textColor,
+      font: temperatureFont
+    });
+  }
+  createWeatherIcon(forecast: WeatherDatum) {
     return new tabris.ImageView({
-      bottom: 0,
-      width: 50,
-      height: 50,
-      centerX: 0,
+      right: "prev()",
+      width: 40,
+      height: 40,
+      centerY: 0,
       image: "/icons/" + forecast.weatherIcon + ".png"
     });
   }
-  createTemperatureText(forecast) {
-    return new tabris.TextView({
-      bottom: "prev() -10",
-      centerX: 0,
-      text: Math.round(forecast.temperature) + "°C",
-      textColor: textColor,
-      font: "bold 28px"
-    });
-  }
+
+
   createSpacer() {
     return new tabris.Composite({
       top: "prev()",
