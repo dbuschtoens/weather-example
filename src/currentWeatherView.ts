6a59df6f24d39d1ed7e257af705b6b8174c9ef7e
David Buschtöns <dbuschtoens@eclipsesource.com>
2016-04-08 10:26:58 +0200

    Remove unused methods and dependency on the height property

--- src/currentWeatherView.ts
+++ src/currentWeatherView.ts
@@ -6,65 +6,29 @@ const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const smallFont = "thin 19px sans-serif";
 const smallFontItalic = "italic thin 19px sans-serif";
 const bigFont = "thin 28px sans-serif";
+const font = "thin 100px sans-serif";
+const iconSize = 100;
 
 interface CurrentWeatherViewProperties extends tabris.CompositeProperties {
   data: WeatherData;
 }
 
 export default class CurrentWeatherView extends tabris.Composite {
-  private font: string;
-  private iconSize: number;
 
   constructor(properties: CurrentWeatherViewProperties) {
     super(properties);
     let data = properties.data;
-    let height = Math.max(200, <number>properties.height); // TODO fix this
-    this.font = "thin " + (height * 0.5) + "px sans-serif";
-    this.iconSize = height * 0.5;
-    let centerBox = new tabris.Composite({
-      top: 0,
-      centerX: 0
-    }).appendTo(this);
+    let centerBox = new tabris.Composite({ top: 0, centerX: 0 }).appendTo(this);
     this.createWeatherIcon(data.list[0].weatherIcon).appendTo(centerBox);
     this.createTemperatureText(Math.round(data.list[0].temperature)).appendTo(centerBox);
     this.createWeatherText(data.list[0].weatherDetailed).appendTo(this);
   }
 
-  private createCityNameText(text: string) {
-    return new tabris.TextView({
-      top: 0,
-      centerX: 0,
-      text: text,
-      textColor: textColor,
-      font: "bold 32px"
-    });
-  }
-
-  private createText(text: string) {
-    return new tabris.TextView({
-      top: "prev()",
-      centerX: 0,
-      text: text,
-      textColor: textColor,
-      font: smallFont
-    });
-  }
-
-  private createItalicText(text: string) {
-    return new tabris.TextView({
-      top: "prev()",
-      centerX: 0,
-      text: text,
-      textColor: textColor,
-      font: "italic " + smallFont
-    });
-  }
-
   private createWeatherIcon(icon: string) {
     return new tabris.ImageView({
       centerY: 0,
-      width: this.iconSize,
-      height: this.iconSize,
+      width: iconSize,
+      height: iconSize,
       scaleMode: "stretch",
       image: "/icons/" + icon + "Big.png"
     });
@@ -76,7 +40,7 @@ export default class CurrentWeatherView extends tabris.Composite {
       left: "prev()",
       text: Math.round(temperature) + "°C",
       textColor: textColor,
-      font: this.font
+      font: font
     });
   }
 
